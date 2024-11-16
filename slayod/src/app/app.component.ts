ee04409a9b9109122b644aab187d15c4c31b3ff9
Kaylesh <Kaylesh.Patel@live.co.uk>
2024-11-16 15:29:45 +0000

    feat: add ng zorro

--- slayod/src/app/app.component.ts
+++ slayod/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as a1lib from 'alt1';
+import {NzContentComponent, NzFooterComponent, NzHeaderComponent, NzLayoutComponent} from 'ng-zorro-antd/layout';
+import {NzTypographyComponent} from 'ng-zorro-antd/typography';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzLayoutComponent, NzHeaderComponent, NzContentComponent, NzFooterComponent, NzTypographyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'slayod';
+  title = 'SLAYoD';
+  footer = 'Version 0. WIP.'
 }
