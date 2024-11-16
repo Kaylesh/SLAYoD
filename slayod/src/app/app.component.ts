c58eefd9a8400d78737e16ef7c755712a5814b0f
Kaylesh <Kaylesh.Patel@live.co.uk>
2024-11-16 20:39:48 +0000

    feat: make simple template, change perms and icon

--- slayod/src/app/app.component.ts
+++ slayod/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NzContentComponent, NzFooterComponent, NzHeaderComponent, NzLayoutComponent} from 'ng-zorro-antd/layout';
 import {NzTypographyComponent} from 'ng-zorro-antd/typography';
-import {NzButtonComponent} from 'ng-zorro-antd/button';
+import {NzButtonComponent, NzButtonGroupComponent} from 'ng-zorro-antd/button';
+import {NzColDirective, NzRowDirective} from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NzLayoutComponent, NzHeaderComponent, NzContentComponent, NzFooterComponent, NzTypographyComponent, NzButtonComponent],
+  imports: [RouterOutlet, NzLayoutComponent, NzHeaderComponent, NzContentComponent, NzFooterComponent, NzTypographyComponent, NzButtonComponent, NzButtonGroupComponent, NzRowDirective, NzColDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,12 +18,14 @@ export class AppComponent {
   testContent = "";
 
   isAlt1Detected = !!window.alt1;
+  isRsActive = false;
 
   constructor() {
     if(this.isAlt1Detected){
       alt1.identifyAppUrl('appconfig.json');
       this.testContent = 'Alt1 detected!'
-      console.log(alt1);
+      this.isRsActive = alt1.rsLinked;
     }
+
   }
 }
