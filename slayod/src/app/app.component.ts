425c57a197b00d82351745d8a9903e41faba1a9a
Kaylesh <Kaylesh.Patel@live.co.uk>
2024-11-16 19:41:38 +0000

    feat: implement connection to alt1

--- slayod/src/app/app.component.ts
+++ slayod/src/app/app.component.ts
@@ -1,17 +1,28 @@
+import * as a1lib from 'alt1';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import * as a1lib from 'alt1';
 import {NzContentComponent, NzFooterComponent, NzHeaderComponent, NzLayoutComponent} from 'ng-zorro-antd/layout';
 import {NzTypographyComponent} from 'ng-zorro-antd/typography';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NzLayoutComponent, NzHeaderComponent, NzContentComponent, NzFooterComponent, NzTypographyComponent],
+  imports: [RouterOutlet, NzLayoutComponent, NzHeaderComponent, NzContentComponent, NzFooterComponent, NzTypographyComponent, NzButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'SLAYoD';
-  footer = 'Version 0. WIP.'
+  testContent = "";
+
+  isAlt1Detected = !!window.alt1;
+
+  constructor() {
+    if(this.isAlt1Detected){
+      alt1.identifyAppUrl('appconfig.json');
+      this.testContent = 'Alt1 detected!'
+      console.log(alt1);
+    }
+  }
 }
